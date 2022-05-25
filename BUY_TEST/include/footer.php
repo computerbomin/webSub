6515084b30223f3e20efd1111ab19831e787946a
computerbomin <89966560+computerbomin@users.noreply.github.com>
2022-05-25 12:39:38 +0900

    쇼핑몰
    DB, 메인까지

---
+++ BUY_TEST/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<section id="footer_in">
+		<div id="footer_logo">
+			<img src="/project/webSub/BUY_TEST/imgs/logo.png" alt="footer_logo" title="footer_logo" />
+		</div>
+		<div id="copyright">
+			Copyright S Shop
+		</div>
+	</section>
+</footer>
---
+++ BUY_TEST/include/header.php
@@ -0,0 +1,42 @@
+<?php
+    include $_SERVER['DOCUMENT_ROOT']."/project/webSub/BUY_TEST/include/db.php";
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8" />
+    <title>땡땡 쇼핑</title>
+    <link rel="stylesheet" href="/project/webSub/BUY_TEST/css/common.css" />
+</head>
+<body>
+<header>
+    <div id="header_in">
+        <div id="logo">
+            <img src="/project/webSub/BUY_TEST/imgs/logo.png" alt="logo" title="logo" />
+        </div>
+        <form action="" method="get">
+            <div id="search_bar">
+                <input type="text" />
+            </div>
+            <div id="search_bt">
+                <img src="/project/webSub/BUY_TEST/imgs/search_bt.png" alt="search_bt" title="search_bt" />
+            </div>
+        </form>
+        <div id="header_in2">
+            <div id="service_menu">
+                <ul>
+                    <li>로그인</li>
+                    <li>회원가입</li>
+                </ul>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">내 정보</a></li>
+                    <li><a href="">주문조회</a></li>
+                    <li><a href="">장바구니</a></li>
+                    <li><a href="">고객센터</a></li>
+                    <li><a href="">로그아웃</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
